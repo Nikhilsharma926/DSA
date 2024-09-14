0278ab2dddefa3c76826fdb8db3a24f7581bed8b
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-14 21:17:10 +0530

    sum of two numbers

---
+++ frth.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout <<"enter the first number :";
+    cin >> a;
+    cout <<"enter the second numder :";
+    cin >> b;
+    int sum = a+b;
+    cout << "sum =" << sum;
+    return 0;
+}
