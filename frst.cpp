36805dc40ac2ef011d4ff5433120b1fb255febaf
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-14 13:40:39 +0530

    syntax

---
+++ frst.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Nikhil \n Sharma";
+    return 0;
+
+}
+//how to run code in c++
+ //step1: g++ file name 
+ // step: ./a
+ // output
