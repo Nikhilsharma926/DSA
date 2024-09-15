6f77309bb87c3c5f30557d064d6ef11aa3011503
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 17:14:43 +0530

    print sum of odd no. using for loop

--- sisth.cpp
+++ sisth.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n =5;
+    int n =5 ,sum =0 ;
     for(int i =1; i<=n; i++){
         if(i%2!=0){
-            cout<<" " <<i;
+           
+          sum = sum + i;
         }
+
     }
+    
+    cout<<" sum of  odd numbers from 1 to "<< n<< " is " <<sum;
 }
