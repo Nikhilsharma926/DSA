ebf928d3e1835c46c954e10cd445e4732f9653c3
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 16:44:31 +0530

    print sum of no. from 1 to n using while loop

---
+++ ffth.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =3, count =1,sum = 0;
+
+    while(  count<=a ){
+    sum=sum+count;
+        count++;
+
+    }
+    cout<<"sum = " <<sum;
+    return 0;   
+    
+    
+}
