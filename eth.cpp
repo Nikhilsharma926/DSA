1cc29483098c93ccbba7590fa6f4b6e7bb81a729
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 10:59:14 +0530

    write a program using if else statements find vote&!vote

---
+++ eth.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter the age:";
+    cin>> age;
+    if (17<=age){
+        cout<<"your are qualified";
+    }
+    
+        else{
+            cout<<"your are not";
+        }
+
+
+    }
+
