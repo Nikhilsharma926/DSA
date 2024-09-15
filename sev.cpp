342e2eccbcc09d668a0792967e795954c9a01d97
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 10:38:56 +0530

    conditonal statements if&else

---
+++ sev.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter any no:";
+    cin>> n;
+    if (n>=0){
+cout<<"the number is positive:";
+    }
+    else {
+        cout<<"neg number:";
+    }
+}
