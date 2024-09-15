fe8085cbc543cbfa59bdb2ef83e4530c90bf123a
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 12:27:07 +0530

    write a program print no. 1 to 10

---
+++ twth.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n =10;
+    int count = 1;
+    while (count<=n)
+    {
+        cout<<count<< "  ";
+        count++;
+    }
+    cout << endl;
+    return 0;
+    
+}
