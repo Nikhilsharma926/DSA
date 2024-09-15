0b162fbc783a783ad1f1553076a9c46b8da24dd9
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 15:56:56 +0530

    print sum of no. from 1 to n

---
+++ forth.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =3;
+    int sum = 0;
+    for(int i =1 ; i<=a; i++){
+        sum = sum +i;
+      
+
+    }
+      cout <<"sum = "<<sum;
+        return 0;
+}
