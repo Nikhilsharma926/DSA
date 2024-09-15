2f4f7637684769ae1a2e223d8a072a9a044aeef7
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-15 11:14:16 +0530

    write a progrm print even & odd no. using conditional statements

---
+++ nth.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter any number:";
+    cin>> n;
+    if (n%2==0){
+        cout<< n << "=" << "is a even no.";
+        
+        }
+        else{
+            cout<< n <<"=" <<"is a odd no.";
+           
+        }
+        return 0;
+
+}
