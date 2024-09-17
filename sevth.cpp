f1d1e9b27f810eca18c50a10ad63bb937866e13a
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-17 17:28:40 +0530

    print no. is prime &not

---
+++ sevth.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the any no:";
+    cin>>n;
+    if(n%4!=0){
+        cout<<"prime";
+    }
+    else{
+        cout<<"not prime";
+    }
+    return 0;
+}
