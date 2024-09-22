907a1fe5b09e7d72a7a101336ce01431e4678cc7
Nikhilsharam962 <niksharma787683@gmail.com>
2024-09-22 17:13:57 +0530

    calculate the sum of no. from 1 TO n

---
+++ function/six.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int sumN(int n){
+    int sum =0;
+    for(int i=1; i<=n; i++){
+         sum+=i;
+    }
+    return sum;
+
+}
+int main(){
+    cout<<sumN(5);
+}
